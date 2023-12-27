14856eec2a7eeb920f85956e71d46c90970402e2
Yi Lin <yiiiliiinn@gmail.com>
2023-12-28 03:05:00 +0800

    fixed lighting

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,28 @@
 import * as THREE from 'three';
 
-// instantiate sphere and scene 
+// scene 
 const scene = new THREE.Scene();
-const geometry = new THREE.SphereGeometry(15, 45, 32, 0, 6.2831, 0, 3.1415);
+
+// sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color : "009387",
-  emissive : "4f00a5",
-  roughness : 0, 
-  metalness : 0.307
+  color: '#00ff83',
 });
-const mesh = new THREE.Mesh( geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// instantiate a camera 
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+// light
+const light = new THREE.PointLight(0xffffff, 80, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+// camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
 camera.position.z = 20;
-scene.add(camera)
+scene.add(camera);
 
 // render scene
 const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(800, 600);
 renderer.render(scene, camera);
